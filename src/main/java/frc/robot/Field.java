3871c43737bc8622b3458f11105541e64bde48a4
kayla <kaylabates856@gmail.com>
2026-01-14 17:13:48 -0600

    gneral field

--- src/main/java/frc/robot/Field.java
+++ src/main/java/frc/robot/Field.java
@@ -1,5 +1,21 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Inches;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Distance;
+
 public class Field {
-    
+    // X is left to right, Y is top to bottom
+
+    public static final Pose2d origin = Pose2d.kZero;
+
+    // Field
+    public static final Distance fieldLength = Inches.of(651.22);
+    public static final Distance fieldWidth = Inches.of(317.69);
+    public static final Pose2d fieldCenter = new Pose2d(fieldLength.div(2), fieldWidth.div(2), Rotation2d.kZero);
+
+    // Blue side
+    public static final Pose2d BlueHub = new Pose2d();
 }
