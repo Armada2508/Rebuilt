c35ab009512b474a7f74dde2e2e6ce9ef4898d92
Kale <kaelgriffin482@gmail.com>
2026-01-15 17:44:05 -0600

    motor configs for intake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,42 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Radians;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+
 public class Constants {
     public static class IntakeK {
-
+        public static final int motorID = 0;
         
+
+        // PID & Feedforward gains
+        public static final double kP = 0; //! find all values
+        public static final double kD = 0;
+        public static final double kV = 0;
+
+        // Limits
+        public static final Angle maxAngle = Degrees.of(0); //! find values
+        public static final Angle minAngle = Degrees.of(0);
+
+
+        public static final Slot0Configs pidConfig = new Slot0Configs()
+        .withKP(kP)
+        .withKD(kD)
+        .withKV(kV);
+
+        public static final SoftwareLimitSwitchConfigs softwareLimitConfig = new SoftwareLimitSwitchConfigs()
+        .withForwardSoftLimitEnable(true)
+        .withReverseSoftLimitEnable(true)
+        .withForwardSoftLimitThreshold(maxAngle.in(Degrees))
+        .withReverseSoftLimitThreshold(maxAngle.in(Degrees));
+
     }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,54 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.Constants.IntakeK;
+
 public class Intake {
-    
+
+    private final TalonFX talonWheels = new TalonFX(IntakeK.talonWheelsID);
+    private final TalonFX talonArm = new TalonFX(IntakeK.talonArmID);
+
+    public Intake() {
+        configTalons();
+        configMotionMagic();
+        
+    }
+
+    public void configTalons() {
+        TalonFXConfiguration wheelsConfig = new TalonFXConfiguration();
+        TalonFXConfiguration armConfig = new TalonFXConfiguration();
+        
+        talonWheels.getConfigurator().apply(new TalonFXConfiguration()); // factory reset
+        talonWheels.getConfigurator().apply(IntakeK.pidConfig);
+        talonWheels.getConfigurator().apply(IntakeK.softwareLimitConfig);
+        talonArm.getConfigurator().apply(new TalonFXConfiguration());
+    }
+    public void configMotionMagic() {
+        
+    }
+
+    public void stow() { // Put the intake up and stop the motors
+
+    }
+
+    public void intakeDepot() { 
+
+    }
+
+    public void setAngle() { // Set the angle to which the intake will move to
+        
+    }
+
+    public void getAngle() { //! change return type?
+
+    }
+
+    public void stop() { // stop everything
+        talonArm.stopMotor();
+        talonWheels.stopMotor();
+    }
+   
 }
