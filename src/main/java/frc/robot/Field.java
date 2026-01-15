241bf00b651938d0aa934ba03ad914f4703d7aa6
chris <christopp1228@gmail.com>
2026-01-14 23:46:55 -0600

    clean up, adjust indent, naming

--- src/main/java/frc/robot/Field.java
+++ src/main/java/frc/robot/Field.java
@@ -20,20 +20,18 @@ public class Field {
     // Blue side
     public static final Pose2d blueHub = new Pose2d(new Translation2d(Inches.of(182.11), Inches.of(158.84)), Rotation2d.k180deg); 
     public static final Pose2d blueTower = new Pose2d(new Translation2d(Inches.of(41.56), Inches.of(147.47)), Rotation2d.kZero);
-    public static final Pose2d blueDepot = new Pose2d(new Translation2d(Inches.of(15.5), Inches.of(234.77)), Rotation2d.kZero); //! This is off
+    public static final Pose2d blueDepot = new Pose2d(new Translation2d(Inches.of(15.5), Inches.of(234.77)), Rotation2d.kZero); 
     public static final Pose2d blueOutpost = new Pose2d(new Translation2d(Inches.of(0), Inches.of(26.22)), Rotation2d.kZero);
-    public static final Pose2d blueTrenchBottom = new Pose2d(new Translation2d(Inches.of(182.11), Inches.of(24.92)), Rotation2d.k180deg);
-    public static final Pose2d blueTrenchTop = new Pose2d(new Translation2d(blueTrenchBottom.getMeasureX(), fieldWidth.minus(blueTrenchBottom.getMeasureY())), Rotation2d.k180deg); //! Bad, use .getMeasureX() / .getMeasureY() instead of Inches.of(...)
+    public static final Pose2d blueTrenchRight = new Pose2d(new Translation2d(Inches.of(182.11), Inches.of(24.92)), Rotation2d.k180deg);
+    public static final Pose2d blueTrenchLeft = new Pose2d(new Translation2d(blueTrenchRight.getMeasureX(), fieldWidth.minus(blueTrenchRight.getMeasureY())), Rotation2d.k180deg); 
 
     // Red side
-    public static final Pose2d redHub = new Pose2d(new Translation2d(fieldLength.minus(blueHub.getMeasureX()), (fieldWidth.minus(blueHub.getMeasureY()))), Rotation2d.kZero); //! Bad, see above, it is the same cause
+    public static final Pose2d redHub = new Pose2d(new Translation2d(fieldLength.minus(blueHub.getMeasureX()), (fieldWidth.minus(blueHub.getMeasureY()))), Rotation2d.kZero); 
     public static final Pose2d redTower = new Pose2d(new Translation2d(fieldLength.minus(blueTower.getMeasureX()), (fieldWidth.minus(blueTower.getMeasureY()))), Rotation2d.k180deg);
     public static final Pose2d redDepot = new Pose2d(new Translation2d(fieldLength.minus(blueDepot.getMeasureX()), (fieldWidth.minus(blueDepot.getMeasureY()))), Rotation2d.k180deg);
     public static final Pose2d redOutpost = new Pose2d(new Translation2d(fieldLength.minus(blueOutpost.getMeasureX()), (fieldWidth.minus(blueOutpost.getMeasureY()))), Rotation2d.k180deg);
-    public static final Pose2d redTrenchTop = new Pose2d(new Translation2d(fieldLength.minus(blueTrenchBottom.getMeasureX()), (fieldWidth.minus(blueTrenchBottom.getMeasureY()))), Rotation2d.kZero);
-    public static final Pose2d redTrenchBottom = new Pose2d(new Translation2d(fieldLength.minus(blueTrenchTop.getMeasureX()), (fieldWidth.minus(blueTrenchTop.getMeasureY()))), Rotation2d.kZero);
-
-
+    public static final Pose2d redTrenchRight = new Pose2d(new Translation2d(fieldLength.minus(blueTrenchRight.getMeasureX()), (fieldWidth.minus(blueTrenchRight.getMeasureY()))), Rotation2d.kZero);
+    public static final Pose2d redTrenchLeft = new Pose2d(new Translation2d(fieldLength.minus(blueTrenchLeft.getMeasureX()), (fieldWidth.minus(blueTrenchLeft.getMeasureY()))), Rotation2d.kZero);
 
 
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,80 +11,79 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    private Command m_autonomousCommand;
 
-  Field2d field = new Field2d();
+    Field2d field = new Field2d();
 
-  public Robot() {
-    SmartDashboard.putData("Arena: ", field);
-   field.getObject("Blue Hub").setPose(Field.blueHub);
-   field.getObject("Blue Tower").setPose(Field.blueTower);
-   field.getObject("Blue Outpost").setPose(Field.blueOutpost);
-   field.getObject("Blue Depot").setPose(Field.blueDepot);
-    field.getObject("Blue Trench Top").setPose(Field.blueTrenchTop);
-   field.getObject("Blue Trench Bottom").setPose(Field.blueTrenchBottom);
-
-   field.getObject("Red Hub").setPose(Field.redHub);
-    field.getObject("Red Tower").setPose(Field.redTower);
-   field.getObject("Red Outpost").setPose(Field.redOutpost);
-   field.getObject("Red Depot").setPose(Field.redDepot);
-   field.getObject("Red Trench Top").setPose(Field.redTrenchTop);
-   field.getObject("Red Trench Bottom").setPose(Field.redTrenchBottom);
-   
-
-
-  }
-
-
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-
-    if (m_autonomousCommand != null) {
-      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    public Robot() {
+        logFieldConstants();
     }
-  }
 
-  @Override
-  public void autonomousPeriodic() {}
+    public void logFieldConstants() {
+        SmartDashboard.putData("Arena: ", field);
+        field.getObject("Blue Hub").setPose(Field.blueHub);
+        field.getObject("Blue Tower").setPose(Field.blueTower);
+        field.getObject("Blue Outpost").setPose(Field.blueOutpost);
+        field.getObject("Blue Depot").setPose(Field.blueDepot);
+        field.getObject("Blue Trench Left").setPose(Field.blueTrenchLeft);
+        field.getObject("Blue Trench Right").setPose(Field.blueTrenchRight);
 
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+        field.getObject("Red Hub").setPose(Field.redHub);
+        field.getObject("Red Tower").setPose(Field.redTower);
+        field.getObject("Red Outpost").setPose(Field.redOutpost);
+        field.getObject("Red Depot").setPose(Field.redDepot);
+        field.getObject("Red Trench Left").setPose(Field.redTrenchLeft);
+        field.getObject("Red Trench Right").setPose(Field.redTrenchRight);
     }
-  }
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-  @Override
-  public void teleopExit() {}
+    @Override
+    public void disabledInit() {}
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void disabledPeriodic() {}
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void disabledExit() {}
 
-  @Override
-  public void testExit() {}
+    @Override
+    public void autonomousInit() {
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().schedule(m_autonomousCommand);
+        }
+    }
+
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void autonomousExit() {}
+
+    @Override
+    public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {}
+
+    @Override
+    public void teleopExit() {}
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void testExit() {}
 }
