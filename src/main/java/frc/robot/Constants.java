be6318007b3cd64d98f1847d986208da929934ff
FarmerOfEggplants <andrewboy1389@gmail.com>
2026-01-15 10:21:26 -0600

    Copy + Pasted most of Reefscape's vision code + constants.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,64 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.FeetPerSecond;
+import static edu.wpi.first.units.Units.FeetPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.math.Matrix;
+// import edu.wpi.first.math.Pair;
+// import edu.wpi.first.math.VecBuilder;
+// import edu.wpi.first.math.geometry.Pose3d;
+// import edu.wpi.first.math.geometry.Rotation3d;
+// import edu.wpi.first.math.geometry.Transform3d;
+// import edu.wpi.first.math.numbers.N1;
+// import edu.wpi.first.math.numbers.N3;
+// import edu.wpi.first.math.trajectory.TrapezoidProfile;
+// import edu.wpi.first.math.util.Units;
+// import edu.wpi.first.units.LinearAccelerationUnit;
+// import edu.wpi.first.units.measure.Angle;
+// import edu.wpi.first.units.measure.AngularAcceleration;
+// import edu.wpi.first.units.measure.AngularVelocity;
+// import edu.wpi.first.units.measure.Current;
+// import edu.wpi.first.units.measure.Distance;
+// import edu.wpi.first.units.measure.LinearAcceleration;
+// import edu.wpi.first.units.measure.LinearVelocity;
+// import edu.wpi.first.units.measure.Time;
+// import edu.wpi.first.units.measure.Voltage;
+// import edu.wpi.first.wpilibj.Filesystem;
+// import frc.robot.lib.util.Encoder;
 public class Constants {
-    
+    public static class VisionK {
+        public static final String frontCameraName = "ArducamFront"; // 7.5, 34.77, 5.22
+        public static final String backCameraName = "ArducamBack"; 
+        public static final Transform3d robotToFrontCamera = new Transform3d(Inches.of(0.577), Inches.of(-1.023), Inches.of(29.223), new Rotation3d(Degrees.of(11.5), Degrees.of(30.75), Degrees.of(5.8)));
+        public static final Transform3d robotToBackCamera = new Transform3d(Inches.of(-3.148), Inches.of(7.729), Inches.of(32.452), new Rotation3d(Degrees.zero(), Degrees.zero(), Degrees.of(-155)));
+        // Acceptable height of pose estimation to consider it a valid pose
+        public static final Distance maxPoseZ = Inches.of(12);
+        public static final Distance minPoseZ = Inches.of(-6);
+        // Used in scaling the standard deviations by average distance to april tags
+        public static final Distance baseLineAverageTagDistance = Inches.of(84);
+        public static final Distance maxAverageTagDistance = Inches.of(160);
+        // Vision Standard Deviations (Meters, Meters, Radians)
+        public static final Matrix<N3, N1> singleTagStdDevs = VecBuilder.fill(Units.feetToMeters(3), Units.feetToMeters(3), Units.degreesToRadians(360));
+        public static final Matrix<N3, N1> multiTagStdDevs = VecBuilder.fill(Units.feetToMeters(1.5), Units.feetToMeters(1.5), Units.degreesToRadians(180));
+        public static final Matrix<N3, N1> untrustedStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+    }
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,5 +1,230 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import frc.robot.Constants.VisionK;
+
+@Logged
 public class Vision {
-    
+    private final PhotonCamera frontCamera = new PhotonCamera(VisionK.frontCameraName);
+    private final PhotonCamera backCamera = new PhotonCamera(VisionK.backCameraName);
+    private final PhotonPoseEstimator frontPoseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionK.robotToFrontCamera);
+    private final PhotonPoseEstimator backPoseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, VisionK.robotToBackCamera);
+    private final NetworkTable table = NetworkTableInstance.getDefault().getTable("Robot").getSubTable("Vision");
+    private final StructPublisher<Pose3d> pubFront = table.getStructTopic(VisionK.frontCameraName + " StdDevs/estimatedPose", Pose3d.struct).publish();
+    private final StructPublisher<Pose3d> pubBack = table.getStructTopic(VisionK.backCameraName + " StdDevs/estimatedPose", Pose3d.struct).publish();
+    private PhotonPipelineResult frontLatestResult;
+    private PhotonPipelineResult backLatestResult;
+
+    @SuppressWarnings("removal")
+    @Override
+    public void periodic() {
+        if (isCameraConnectedFront()) {
+            frontLatestResult = frontCamera.getLatestResult();
+        }
+        if (isCameraConnectedBack()) {
+            backLatestResult = backCamera.getLatestResult();
+        }
+    }
+
+    /**
+     * Returns a wrapper object containing a list of estimated robot poses and their standard deviations stored as a Pair to be added into 
+     * a robot's odometry with PoseEstimator#addVisionMeasurement.
+     */
+    public VisionResults getVisionResults() {
+        List<Pair<EstimatedRobotPose, Matrix<N3, N1>>> visionResults = new ArrayList<>();
+        if (isCameraConnectedFront()) {
+            visionResults.addAll(processResults(frontCamera.getAllUnreadResults(), frontPoseEstimator, frontCamera.getName()));
+        }
+        if (isCameraConnectedBack()) {
+            visionResults.addAll(processResults(backCamera.getAllUnreadResults(), backPoseEstimator, backCamera.getName()));
+        }
+        return new VisionResults(visionResults);
+    }
+
+    /**
+     * Processes a list of photonvison results into a list of estimated poses and their respective standard deviations
+     */
+    private List<Pair<EstimatedRobotPose, Matrix<N3, N1>>> processResults(List<PhotonPipelineResult> results, PhotonPoseEstimator poseEstimator, String name) {
+        List<Pair<EstimatedRobotPose, Matrix<N3, N1>>> visionResults = new ArrayList<>();
+        for (var result : results) {
+            poseEstimator.update(result).ifPresent((pose) -> {
+                if (isValidPose(pose)) {
+                    var stdDevs = getStdDevs(result, pose, poseEstimator, name);
+                    table.getEntry(name + " StdDevs/Standard Deviations").setDoubleArray(stdDevs.getData());
+                    visionResults.add(Pair.of(pose, stdDevs));
+                }
+            });
+        }
+        return visionResults;
+    }
+
+    /**
+     * Checks whether a pose is within the bounds of the field and the acceptable Z (height) error
+     */
+    private boolean isValidPose(EstimatedRobotPose pose) {
+        var pose3d = pose.estimatedPose;
+        return pose3d.getMeasureX().gte(Meters.zero()) 
+            && pose3d.getMeasureX().lte(Field.fieldLength)
+            && pose3d.getMeasureY().gte(Meters.zero())
+            && pose3d.getMeasureY().lte(Field.fieldWidth)
+            && pose3d.getMeasureZ().gte(VisionK.minPoseZ)
+            && pose3d.getMeasureZ().lte(VisionK.maxPoseZ);
+    }
+
+    /**
+     * Returns the standard deviations for a given photonvision result and estimated pose
+     */
+    private Matrix<N3, N1> getStdDevs(PhotonPipelineResult result, EstimatedRobotPose pose, PhotonPoseEstimator poseEstimator, String name) {
+        int numTags = 0;
+        double avgDistMeters = 0; 
+        for (var target : result.getTargets()) {
+            var tagPose = poseEstimator.getFieldTags().getTagPose(target.getFiducialId());
+            if (tagPose.isEmpty()) continue;
+            numTags++;
+            avgDistMeters += tagPose.get().toPose2d().getTranslation().getDistance(pose.estimatedPose.getTranslation().toTranslation2d());
+        }
+        avgDistMeters /= numTags;
+        double stdevScalar = avgDistMeters / VisionK.baseLineAverageTagDistance.in(Meters);
+        // Logging
+        if (name == VisionK.frontCameraName) pubFront.accept(pose.estimatedPose);
+        else pubBack.accept(pose.estimatedPose);
+        table.getEntry(name + " StdDevs/numTags").setInteger(numTags);
+        table.getEntry(name + " StdDevs/Average Distance to Tag (in.) RF").setDouble(Units.metersToInches(avgDistMeters)); // Robot Frame
+        table.getEntry(name + " StdDevs/stdevScalar").setDouble(stdevScalar);
+        if (avgDistMeters > VisionK.maxAverageTagDistance.in(Meters)) {
+            return VisionK.untrustedStdDevs;
+        }
+        if (numTags == 0) return VisionK.untrustedStdDevs;
+        boolean sawReef = false;
+        for (var target : result.getTargets()) {
+            if (VisionK.reefTags.contains(target.getFiducialId())) {
+                sawReef = true;
+            }
+        }
+        Matrix<N3, N1> stdDevs;
+        if (numTags == 1) {
+            stdDevs = VisionK.singleTagStdDevs.times(stdevScalar);
+        }
+        stdDevs = VisionK.multiTagStdDevs.times(stdevScalar);
+        if (name == VisionK.backCameraName && !sawReef) {
+            stdDevs = stdDevs.times(4);
+        }
+        return stdDevs;
+    }
+    @Logged(name = "Front Camera Connected")
+    public boolean isCameraConnectedFront() {
+        return frontCamera.isConnected();
+    } 
+
+    /**
+     * Returns whether the back camera is connected
+     */
+    @Logged(name = "Back Camera Connected")
+    public boolean isCameraConnectedBack() {
+        return backCamera.isConnected();
+    } 
+
+    /**
+     * Returns whether the front camera can see an april tag
+     */
+    @Logged(name = "Front Camera Sees Tag")
+    public boolean canSeeTagFront() {
+        if (!isCameraConnectedFront()) return false;
+        return frontLatestResult.hasTargets();    
+    }
+
+    /**
+     * Returns whether the back camera can see an april tag
+     */
+    @Logged(name = "Back Camera Sees Tag")
+    public boolean canSeeTagBack() {
+        if (!isCameraConnectedBack()) return false;
+        return backLatestResult.hasTargets();    
+    }
+
+    /**
+     * Returns the ID of the best april tag seen by the front camera or -1 if no tag is seen
+     */
+    @Logged(name = "Front Camera Best Tag ID")
+    public int bestTagIDFront() {
+        if (!canSeeTagFront()) return -1;
+        return frontLatestResult.getBestTarget().getFiducialId();
+    }
+
+    /**
+     * Returns the ID of the best april tag seen by the back camera  or -1 if no tag is seen
+     */
+    @Logged(name = "Back Camera Best Tag ID")
+    public int bestTagIDBack() {
+        if (!canSeeTagBack()) return -1;
+        return backLatestResult.getBestTarget().getFiducialId();
+    }
+
+    /**
+     * Returns the number of april tags seen by the front camera
+     */
+    @Logged(name = "Front Camera Number Tags Seen")
+    public int numTagsSeenFront() {
+        if (!canSeeTagFront()) return 0;
+        return frontLatestResult.getTargets().size();
+    }
+
+    /**
+     * Returns the number of april tags seen by the back camera
+     */
+    @Logged(name = "Back Camera Number Tags Seen")
+    public int numTagsSeenBack() {
+        if (!canSeeTagBack()) return 0;
+        return backLatestResult.getTargets().size();
+    }
+
+    /**
+     * Returns the normal distance to the best tag in inches from the front camera (Camera Frame) or -1 if no tag is seen
+     */
+    @Logged(name = "Front Camera Normal Distance to Best Tag")
+    public double distanceToBestTagFront() {
+        if (!canSeeTagFront()) return -1;
+        return Units.metersToInches(frontLatestResult.getBestTarget().getBestCameraToTarget().getTranslation().getNorm());
+    }
+
+    /**
+     * Returns the normal distance to the best tag in inches from the back camera (Camera Frame) or -1 if no tag is seen
+     */
+    @Logged(name = "Back Camera Normal Distance to Best Tag")
+    public double distanceToBestTagBack() {
+        if (!canSeeTagBack()) return -1;
+        return Units.metersToInches(backLatestResult.getBestTarget().getBestCameraToTarget().getTranslation().getNorm());
+    }
+
+    /**
+     * Returns the normal distance to the best tag in inches from the front camera (Camera Frame) or -1 if no tag is seen
+     */
+    @Logged(name = "Robot Normal Distance to Front Best Tag")
+    public double robotToFrontTag() {
+        if (!canSeeTagFront()) return -1;
+        return Units.metersToInches(
+            Pose3d.kZero.transformBy(frontLatestResult.getBestTarget().getBestCameraToTarget().inverse())
+            .transformBy(VisionK.robotToFrontCamera.inverse()).getTranslation().toTranslation2d().getNorm()
+        );
+    }
+    // This is your poor man's type alias, allows me to shorten the type and reference it by using VisionResults instead of List<Pair<EstimatedRobotPose, Matrix<N3, N1>>>
+    public record VisionResults(List<Pair<EstimatedRobotPose, Matrix<N3, N1>>> results){}
 }
