7cebbe94265dbbc01db8a9aa1e4b159f336a90a3
kayla <kaylabates856@gmail.com>
2026-01-14 18:00:27 -0600

    general field

--- src/main/java/frc/robot/Field.java
+++ src/main/java/frc/robot/Field.java
@@ -4,7 +4,9 @@ import static edu.wpi.first.units.Units.Inches;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Field {
     // X is left to right, Y is top to bottom
@@ -17,5 +19,18 @@ public class Field {
     public static final Pose2d fieldCenter = new Pose2d(fieldLength.div(2), fieldWidth.div(2), Rotation2d.kZero);
 
     // Blue side
-    public static final Pose2d BlueHub = new Pose2d();
+    public static final Pose2d BlueHub = new Pose2d(new Translation2d(Inches.of(182.11), Inches.of(158.84)), Rotation2d.kZero);
+    public static final Pose2d BlueTower = new Pose2d(new Translation2d(Inches.of(41.56), Inches.of(147.47)), Rotation2d.kZero);
+    public static final Pose2d BlueDepot = new Pose2d(new Translation2d(Inches.of(15.5), Inches.of(241.75)), Rotation2d.kZero);
+    public static final Pose2d BlueOutpost = new Pose2d(new Translation2d(Inches.of(0), Inches.of(26.22)), Rotation2d.kZero);
+    public static final Pose2d BlueTrenchBottom = new Pose2d(new Translation2d(Inches.of(182.11), Inches.of(24.92)), Rotation2d.kZero);
+    public static final Pose2d BlueTrenchTop = new Pose2d(new Translation2d(Inches.of(BlueTrenchBottom.getX()), fieldWidth.minus(Inches.of(BlueTrenchBottom.getY()))), Rotation2d.kZero);
+
+    // Red side
+
+    public static final Pose2d RedHub = new Pose2d(new Translation2d(fieldLength.minus(Inches.of(BlueHub.getX())), (fieldWidth.minus(Inches.of(BlueHub.getY())))), Rotation2d.kZero);
+
+
+
+
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,14 +5,24 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
+  Field2d field = new Field2d();
 
   public Robot() {
+    SmartDashboard.putData("Arena: ", field);
+   field.getObject("Blue Hub").setPose(Field.BlueHub);
+   field.getObject("Blue Tower").setPose(Field.BlueLadder);
+   field.getObject("Blue Outpost").setPose(Field.BlueOutpost);
+   field.getObject("Blue Depot").setPose(Field.BlueDepot);
+   
+   
   }
 
 
