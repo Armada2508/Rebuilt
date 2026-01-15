01e1c3da7fd2d536f9e5e9a68353970662cedb73
ADavis012 <davisaud000@stillwaterschools.org>
2026-01-15 17:48:51 -0600

    Making commands for shooter voltage, stopping, ect.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,16 @@
 package frc.robot;
+
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.units.measure.Voltage;
+
 public class Constants {
 
-    public static class ShooterK {
+    public static class ShooterK { //! find motor ID and proper measurements
         public static final int talonID = 0;
+        public static final Voltage fuelShootVoltage = Volts.of(1);
+        public static final Time flywheelSpeedUpTime = Seconds.of(0.5);
     }
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,22 +1,67 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.StaticBrake;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterK;
+import frc.robot.lib.util.Util;
 
+@Logged
 public class Shooter extends SubsystemBase{
     
     private final TalonFX talonShooter = new TalonFX(ShooterK.talonID);
 
     public Shooter(){
+        configTalons();
     }
 
+    /**
+     * The talons NeutralMode need to be set as coastMode for the longevity of the motor
+     * (brakeMode may cause damage)
+     */
     public void configTalons(){
+        Util.factoryReset(talonShooter);
+        Util.coastMode(talonShooter);
     }
 
-    public Command
+    public Command setShooterVoltage(Voltage voltage){
+        return runOnce(() -> {
+            talonShooter.setControl(new VoltageOut(voltage.in(Volts)));
+        }).withName("Set Shooter Voltage");
+    }
 
+    //public Command brakeShooter(){
+    //    return runOnce(() -> {
+    //        var request = new StaticBrake();
+    //        talonShooter.setControl(request);
+    //    }).withName("Brake Shooter");
+    //}
 
+    /**
+     * Sets shooter motor to its NeutralMode
+     * (in this case coastMode)
+     */
+    public void stop(){
+        talonShooter.setControl(new NeutralOut());
+    }
+
+    public Command flywheelUpToSpeed(){
+        return setShooterVoltage(ShooterK.fuelShootVoltage)
+        .andThen(Commands.waitSeconds(ShooterK.flywheelSpeedUpTime));
+    }
+
+    //!unfinished, figure it out soon
+    public Command shootFuel(){
+        return setShooterVoltage(ShooterK.fuelShootVoltage)
+        .andThen();
+    }
 }
