ec41cf1ce54d0b08233d324d58ace151879b95bf
kayla <kaylabates856@gmail.com>
2026-01-16 18:56:59 -0600

    more config + methods

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,43 +1,62 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Inches;
-
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
-import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
-import edu.wpi.first.units.AngleUnit;
 import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+
 
 public class Constants {
     public static class IntakeK {
-        public static final int talonArmID = 0;
-        public static final int talonWheelsID = 1;
+        public static final int armID = 0;
+        public static final int wheelsID = 1;
+        TalonFXConfiguration wheelsConfig = new TalonFXConfiguration();
+        TalonFXConfiguration armConfig = new TalonFXConfiguration();
         
-
         // PID & Feedforward gains
+        
         public static final double kP = 0; //! find all values
         public static final double kD = 0;
         public static final double kV = 0;
+        public static final double kG = 0;
 
         // Limits
         public static final Angle maxAngle = Degrees.of(0); //! find values
         public static final Angle minAngle = Degrees.of(0);
-
+        public static final Angle stowAngle = Degrees.of(0);
+        public static final Angle intakeDepotAngle = Degrees.of(0);
+        public static final Angle intakeAngle = Degrees.of(0);
+        public static final Voltage spinWheelsVoltage = Volts.of(0);
 
         public static final Slot0Configs pidConfig = new Slot0Configs()
         .withKP(kP)
         .withKD(kD)
-        .withKV(kV);
+        .withKV(kV)
+        .withKG(kG);
+
+        public static final AngularVelocity maxVelocity = DegreesPerSecond.of(0);
+        public static final AngularAcceleration maxAcceleration = DegreesPerSecondPerSecond.of(0);
+        // Motion Magic settings
+        
 
         public static final SoftwareLimitSwitchConfigs softwareLimitConfig = new SoftwareLimitSwitchConfigs()
         .withForwardSoftLimitEnable(true)
         .withReverseSoftLimitEnable(true)
         .withForwardSoftLimitThreshold(maxAngle.in(Degrees))
         .withReverseSoftLimitThreshold(maxAngle.in(Degrees));
+
+        
+
+        
 
 
     }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,56 +1,76 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.units.BaseUnits;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Velocity;
 import frc.robot.Constants.IntakeK;
+import frc.robot.lib.util.Util;
 
 public class Intake {
 
-    private final TalonFX talonWheels = new TalonFX(IntakeK.talonWheelsID);
-    private final TalonFX talonArm = new TalonFX(IntakeK.talonArmID);
+    private final TalonFX wheels = new TalonFX(IntakeK.wheelsID);
+    private final TalonFX arm = new TalonFX(IntakeK.armID);
 
     public Intake() {
         configTalons();
-        configMotionMagic();
-        
+        configMotionMagic(IntakeK.maxVelocity, IntakeK.maxAcceleration);
+        setAngle(IntakeK.intakeAngle);
     }
 
     public void configTalons() {
-        TalonFXConfiguration wheelsConfig = new TalonFXConfiguration();
-        TalonFXConfiguration armConfig = new TalonFXConfiguration();
-        
-        talonWheels.getConfigurator().apply(new TalonFXConfiguration()); // factory reset
-        talonWheels.getConfigurator().apply(IntakeK.pidConfig);
-        talonWheels.getConfigurator().apply(IntakeK.softwareLimitConfig);
-        talonArm.getConfigurator().apply(new TalonFXConfiguration());
-        talonArm.getConfigurator().apply(IntakeK.pidConfig);
-        talonArm.getConfigurator().apply(IntakeK.softwareLimitConfig);
+        Util.factoryReset(wheels, arm); // factory reset
+        wheels.getConfigurator().apply(IntakeK.pidConfig);
+        wheels.getConfigurator().apply(IntakeK.softwareLimitConfig);
+
+        arm.getConfigurator().apply(IntakeK.pidConfig);
+        arm.getConfigurator().apply(IntakeK.softwareLimitConfig);
     }
-    public void configMotionMagic() {
+
+    public void configMotionMagic(AngularVelocity velocity, AngularAcceleration acceleration) {
+        MotionMagicConfigs motionMagicConfigs = new MotionMagicConfigs();
+        motionMagicConfigs.MotionMagicCruiseVelocity = velocity.in(DegreesPerSecond);
+        motionMagicConfigs.MotionMagicAcceleration = acceleration.in(DegreesPerSecondPerSecond);
         
+        arm.getConfigurator().apply(motionMagicConfigs);
     }
 
     public void stow() { // Put the intake up and stop the motors
-
+       setAngle(IntakeK.stowAngle);
     }
 
     public void intakeDepot() { 
-
+        setAngle(IntakeK.intakeDepotAngle);
     }
 
-    public void setAngle() { // Set the angle to which the intake will move to
-        
+    public void setAngle(Angle angle) { // Set the angle to which the intake will move to
+        MotionMagicVoltage request = new MotionMagicVoltage(angle);
+        arm.setControl(request);
     }
 
-    public void getAngle() { //! change return type?
+    public Angle getAngle() {
+    return arm.getPosition();
+    }
 
+    public void spinWheels() {
+        wheels.setControl(new VoltageOut(IntakeK.spinWheelsVoltage)); //! find value
     }
 
     public void stop() { // stop everything
-        talonArm.stopMotor();
-        talonWheels.stopMotor();
+        wheels.setControl(new NeutralOut());
+        arm.setControl(new NeutralOut());
     }
    
 }
