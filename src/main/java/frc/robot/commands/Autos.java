c97554e82ca7ef23bae890be180057153fb7f715
ADavis012 <davisaud000@stillwaterschools.org>
2026-03-25 16:03:31 -0500

    changed autos to match whats in winona a bit more

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -10,22 +10,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.shooting.Shooter;
 
 public class Autos {
     
     private Autos(){}
 
-    public static SendableChooser<Command> initPathPlanner(Shooter shooter, Intake intake, Indexer indexer){
+    public static SendableChooser<Command> initPathPlanner(Swerve swerve, Shooter shooter, Intake intake, Indexer indexer){
         FollowPathCommand.warmupCommand().schedule();
         
 
-        NamedCommands.registerCommand("Shoot Fuel", Routines.shoot(shooter, indexer));
+        NamedCommands.registerCommand("Shoot Fuel", Routines.shootInterpolatedRpm(swerve, shooter));
         NamedCommands.registerCommand("Stop Intaking", Routines.stopIntake(intake));
 
         new EventTrigger("intake fuel").onTrue(Routines.intake(intake));
         new EventTrigger("stop intaking").onTrue(Routines.stopIntake(intake));
-        new EventTrigger("shoot fuel").onTrue(Routines.shoot(shooter, indexer));
+        new EventTrigger("shoot fuel").onTrue(Routines.shootInterpolatedRpm(swerve, shooter));
 
         SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
         SmartDashboard.putData("Auto Chooser", autoChooser);
